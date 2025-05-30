0be373d10f67d4a10b218f5a5109fe01f50c85ad
sprovider90 <sprovider90@163.com>
2025-05-30 10:28:56 +0800

    debug

--- auth/manager.go
+++ auth/manager.go
@@ -14,39 +14,39 @@ func newAuth(algorithm string) Auth {
 		return nil
 	}
 }
-func GinInterceptor(ctx *gin.Context, mockAuth Auth) {
+func GinInterceptor() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if utils.PathContainsKey(ctx.Request.URL.Path, "admin") {
+			userToken := ctx.GetHeader("sign_data")
+			apiServerName := ctx.GetHeader("server_name")
+			address := ctx.GetHeader("address")
+			apiMethod := ctx.Request.Method
+			apiPath := ctx.Request.URL.Path
+			algorithmAuth := newAuth("admin")
 
-	if utils.PathContainsKey(ctx.Request.URL.Path, "admin") {
-		userToken := ctx.GetHeader("sign_data")
-		apiServerName := ctx.GetHeader("server_name")
-		address := ctx.GetHeader("address")
-		apiMethod := ctx.Request.Method
-		apiPath := ctx.Request.URL.Path
-		algorithmAuth := newAuth("admin")
-		if mockAuth != nil {
-			algorithmAuth = mockAuth
-		}
-		flag, err := algorithmAuth.Check(userToken, apiServerName, apiMethod, apiPath, address)
-		if err != nil {
-			ctx.JSON(http.StatusOK, map[string]interface{}{
-				"code":  300500,
-				"msg":   "interNet" + err.Error(),
-				"error": "",
-			})
-			ctx.Abort()
-			return
-		}
-		if !flag {
-			ctx.JSON(http.StatusOK, map[string]interface{}{
-				"code":  300501,
-				"msg":   "Auth Failed",
-				"error": "",
-			})
-			ctx.Abort()
-			return
+			flag, err := algorithmAuth.Check(userToken, apiServerName, apiMethod, apiPath, address)
+			if err != nil {
+				ctx.JSON(http.StatusOK, map[string]interface{}{
+					"code":  300500,
+					"msg":   "interNet" + err.Error(),
+					"error": "",
+				})
+				ctx.Abort()
+				return
+			}
+			if !flag {
+				ctx.JSON(http.StatusOK, map[string]interface{}{
+					"code":  300501,
+					"msg":   "Auth Failed",
+					"error": "",
+				})
+				ctx.Abort()
+				return
+			}
+
 		}
 
+		ctx.Next()
 	}
 
-	ctx.Next()
 }
